f86c91469f3e9e66ef90f7b07d1d884a1825af77
Danny Duong <dannyduong4.00@outlook.com>
2023-03-27 13:21:00 -0600

    Change router type

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { createHashRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createHashRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
 import Projects from './pages/Projects';
 import RootLayout from './layouts/RootLayout';
 
-const router = createHashRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
